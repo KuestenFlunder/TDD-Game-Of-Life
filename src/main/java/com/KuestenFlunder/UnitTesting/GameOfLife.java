1f5b47239f2a2914c5b1d7f12bdd546c30151874
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-10-10 22:52:15 +0200

    refactor magic numbers

--- src/main/java/com/KuestenFlunder/UnitTesting/GameOfLife.java
+++ src/main/java/com/KuestenFlunder/UnitTesting/GameOfLife.java
@@ -3,8 +3,18 @@ package com.KuestenFlunder.UnitTesting;
 import java.util.List;
 
 public class GameOfLife {
+
+
+    private static final int TOP_CONDITIONS = 3;
+    private static final int GOOD_CONDITIONS = 2;
+
+
     private static long countLivingNeighbors(List<Boolean> neighborsState) {
         return neighborsState.stream().filter(value -> value).count();
+    }
+
+    private static void validateNeighborsListLenghtIsEight(List<Boolean> neighborsState) {
+        if (neighborsState.size() != 8) throw new IllegalArgumentException("There should be 8 neighbours!");
     }
 
     public CellState checkState(CellState actualState, List<Boolean> neighbors) {
@@ -12,14 +22,13 @@ public class GameOfLife {
         long numberOfLivingNeighbors = countLivingNeighbors(neighbors);
 
         return switch ((int) numberOfLivingNeighbors) {
-            case 2 -> actualState;
-            case 3 -> CellState.ALIVE;
+
+            case GOOD_CONDITIONS -> actualState;
+            //dead cells become alive
+            case TOP_CONDITIONS -> CellState.ALIVE;
+
             default -> CellState.DEAD;
         };
-    }
-
-    private static void validateNeighborsListLenghtIsEight(List<Boolean> neighborsState) {
-        if (neighborsState.size() != 8) throw new IllegalArgumentException("There should be 8 neighbours!");
     }
 
     public enum CellState {
