ee5c09930a3d74f8f82694c6ddee3baa201ab684
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-10-10 23:31:21 +0200

    add/Cell with Point and CellState

--- src/main/java/com/KuestenFlunder/UnitTesting/Cell.java
+++ src/main/java/com/KuestenFlunder/UnitTesting/Cell.java
@@ -3,6 +3,28 @@ package com.KuestenFlunder.UnitTesting;
 import java.awt.*;
 
 public class Cell {
+    private Point point;
+    private CellState cellState;
 
+    public Cell(Point point) {
+        this.point = point;
+        this.cellState = CellState.DEAD;
+    }
 
+    public Cell(Point point, CellState cellState) {
+        this.point = point;
+        this.cellState = cellState;
+    }
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public CellState getCellState() {
+        return cellState;
+    }
+
+    public void setCellState(CellState cellState) {
+        this.cellState = cellState;
+    }
 }
