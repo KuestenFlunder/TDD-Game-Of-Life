cfac188c51ef8b82b7ee0f88517cd1ee43163f92
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-10-15 20:46:09 +0200

    add/inital playground state

--- src/main/java/com/KuestenFlunder/GameOfLife/UnitTestingApplication.java
+++ src/main/java/com/KuestenFlunder/GameOfLife/UnitTestingApplication.java
@@ -2,6 +2,7 @@ package com.KuestenFlunder.GameOfLife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UnitTestingApplication {
@@ -10,4 +11,13 @@ public class UnitTestingApplication {
 		SpringApplication.run(UnitTestingApplication.class, args);
 	}
 
+	@Bean
+	public Playground playground() {
+		Playground playground = new Playground(10, 10);
+		//add blicker pattern to playground
+		playground.getCellByCoordinates(2, 1).setCellState(CellState.ALIVE);
+		playground.getCellByCoordinates(2, 2).setCellState(CellState.ALIVE);
+		playground.getCellByCoordinates(2, 3).setCellState(CellState.ALIVE);
+		return playground;
+	}
 }
